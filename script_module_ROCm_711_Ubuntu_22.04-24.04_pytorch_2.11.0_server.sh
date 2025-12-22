b2d70f74ab4f729aae1a0e0d0972c6899b1a5d40
Jörg Roskowetz <141847198+JoergR75@users.noreply.github.com>
2025-12-22 15:00:01 +0100

    Update and rename script_module_ROCm_711_Ubuntu_22.04-24.04_pytorch_2.10.0_server.sh to script_module_ROCm_711_Ubuntu_22.04-24.04_pytorch_2.11.0_server.sh

--- script_module_ROCm_711_Ubuntu_22.04-24.04_pytorch_2.11.0_server.sh
+++ script_module_ROCm_711_Ubuntu_22.04-24.04_pytorch_2.11.0_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # ================================================================================================================
-# ROCm 7.1.1 + OpenCL 2.x + PyTorch 2.10.0 (nightly for ROCm7.1) + Transformers + Docker Setup
+# ROCm 7.1.1 + OpenCL 2.x + PyTorch 2.11.0 (nightly for ROCm7.1) + Transformers + Docker Setup
 # Compatible with Ubuntu 22.04.x and 24.04.x (Desktop & Server) — Ubuntu 20.04.x is no longer supported
 # ================================================================================================================
 # Description:
-# This script automates the installation of AMD ROCm 7.1.1, PyTorch 2.10.0 (nightly for ROCm7.1), Transformers, and Docker
+# This script automates the installation of AMD ROCm 7.1.1, PyTorch 2.11.0 (nightly for ROCm7.1), Transformers, and Docker
 # on Ubuntu 22.04.x and 24.04.x systems. It automatically fetches the appropriate installation scripts and performs
 # a fully non-interactive setup optimized for both desktop and server environments.
 # ================================================================================================================
@@ -28,7 +28,7 @@
 # ROCm Release Notes:    https://rocm.docs.amd.com/en/docs-7.1.1/about/release-notes.html
 # ROCm Driver Repo:      https://repo.radeon.com/amdgpu-install/7.1.1/ubuntu/
 #
-# PyTorch:               2.10.0.dev20251123+rocm7.0
+# PyTorch:               2.11.0.dev20251221+rocm7.1
 # Transformers:          4.57.3
 # Docker:                min. 28.5.0 (the script will verify and skip installation if minimum requirements are installed)
 #
@@ -47,7 +47,7 @@
 # ---------------------------------------------------------------------------------------------------------------
 # Author:                Joerg Roskowetz
 # Estimated Runtime:     ~15 minutes (depending on system performance and internet speed)
-# Last Updated:          December 12th, 2025
+# Last Updated:          December 22th, 2025
 # ================================================================================================================
 
 # global stdout method
@@ -56,7 +56,7 @@ function print () {
 }
 
 clear &&
-print '\n🚀 ROCm 7.1.1 + OpenCL 2.x + PyTorch 2.10.0 (nightly for ROCm7.1) + Transformers + Docker Setup\nCompatible with Ubuntu 22.04.x and 24.04.x (Desktop & Server)\n⚠️ Ubuntu 20.04.x is no longer supported\n'
+print '\n🚀 ROCm 7.1.1 + OpenCL 2.x + PyTorch 2.11.0 (nightly for ROCm7.1) + Transformers + Docker Setup\nCompatible with Ubuntu 22.04.x and 24.04.x (Desktop & Server)\n⚠️ Ubuntu 20.04.x is no longer supported\n'
 print '\nUbuntu OS Update ...\n'
 
 sudo apt-get update
@@ -142,7 +142,7 @@ install_jellyfish() {
     echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
     source ~/.bashrc
 
-    print '\nInstalling Pytorch 2.10.0 (nightly for ROCm7), Transformers environment ...\n'
+    print '\nInstalling Pytorch 2.11.0 (nightly for ROCm7.1), Transformers environment ...\n'
 
     python3 -m pip install --upgrade pip --quiet --no-input
     python3 -m pip install --upgrade pip wheel --quiet --no-input
@@ -229,7 +229,7 @@ install_noble() {
     echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
     source ~/.bashrc
 
-    print '\nInstalling Pytorch 2.10.0 (nightly for ROCm7), Transformers environment ...\n'
+    print '\nInstalling Pytorch 2.11.0 (nightly for ROCm7.1), Transformers environment ...\n'
 
     pip3 install --upgrade pip --break-system-packages
     pip3 install --upgrade pip wheel --break-system-packages
@@ -322,7 +322,7 @@ print(c)
 EOF
 
 set -e
-MIN_DOCKER_VERSION="25.0.0"
+MIN_DOCKER_VERSION="29.0.0"
 
 # Function to compare Docker versions
 version_ge() {
@@ -362,7 +362,7 @@ else
 fi
 
 # Final installation message
-print '\n✅ Finished ROCm 7.1.1 + OCL 2.x + PyTorch 2.10.0 (nightly for ROCm7.1) + Transformers environment installation and setup.\n'
+print '\n✅ Finished ROCm 7.1.1 + OCL 2.x + PyTorch 2.11.0 (nightly for ROCm7.1) + Transformers environment installation and setup.\n'
 
 # Post-reboot testing instructions
 printf "\nAfter the reboot, test your installation with:\n"
